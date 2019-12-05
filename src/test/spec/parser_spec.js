07e7bada4657cbc3308b8a8de14f253de5da2c2f
hmwong237 <55143752+hmwong237@users.noreply.github.com>
2019-12-05 09:51:38 +0800

    update test case

--- src/test/spec/parser_spec.js
+++ src/test/spec/parser_spec.js
@@ -26,9 +26,9 @@ describe("Parser", function() {
 		
 		expect(arr[0].name).toEqual("A");
 
-		expect(arr[0].attrs.attr1.name).toEqual("attr1");
-		expect(arr[0].attrs.attr1.type).toEqual("int");
-		expect(arr[0].attrs.attr1.modifier).toEqual("+");
+		expect(arr[0].attrs.$attr1.name).toEqual("attr1");
+		expect(arr[0].attrs.$attr1.type).toEqual("int");
+		expect(arr[0].attrs.$attr1.modifier).toEqual("+");
 	});
 	
 
@@ -42,10 +42,10 @@ describe("Parser", function() {
 	
 	expect(arr[0].name).toEqual("B");
 
-	expect(arr[0].methods.method1.name).toEqual("method1");
-	expect(arr[0].methods.method1.type).toEqual("void");
-	expect(arr[0].methods.method1.modifier[0]).toEqual("+");
-	expect(arr[0].methods.method1.parameters.length).toEqual(0);
+	expect(arr[0].methods.$method1.name).toEqual("method1");
+	expect(arr[0].methods.$method1.type).toEqual("void");
+	expect(arr[0].methods.$method1.modifier[0]).toEqual("+");
+	expect(arr[0].methods.$method1.parameters.length).toEqual(0);
 	
 	});
 
@@ -59,16 +59,16 @@ describe("Parser", function() {
 
 	expect(arr[0].name).toEqual("A");
 
-	expect(arr[0].attrs.attr1.name).toEqual("attr1");
-	expect(arr[0].attrs.attr1.type).toEqual("int");
-	expect(arr[0].attrs.attr1.modifier).toEqual("+");
+	expect(arr[0].attrs.$attr1.name).toEqual("attr1");
+	expect(arr[0].attrs.$attr1.type).toEqual("int");
+	expect(arr[0].attrs.$attr1.modifier).toEqual("+");
 
 	expect(arr[1].name).toEqual("B");
 
-	expect(arr[1].methods.method1.name).toEqual("method1");
-	expect(arr[1].methods.method1.type).toEqual("void");
-	expect(arr[1].methods.method1.modifier[0]).toEqual("+");
-	expect(arr[1].methods.method1.parameters.length).toEqual(0);
+	expect(arr[1].methods.$method1.name).toEqual("method1");
+	expect(arr[1].methods.$method1.type).toEqual("void");
+	expect(arr[1].methods.$method1.modifier[0]).toEqual("+");
+	expect(arr[1].methods.$method1.parameters.length).toEqual(0);
 	
 	});
 
@@ -84,17 +84,17 @@ describe("Parser", function() {
 
 	expect(arr[0].name).toEqual("A");
 
-	expect(arr[0].attrs.attr1.name).toEqual("attr1");
-	expect(arr[0].attrs.attr1.type).toEqual("int");
-	expect(arr[0].attrs.attr1.modifier).toEqual("+");
+	expect(arr[0].attrs.$attr1.name).toEqual("attr1");
+	expect(arr[0].attrs.$attr1.type).toEqual("int");
+	expect(arr[0].attrs.$attr1.modifier).toEqual("+");
 
 	expect(arr[1].name).toEqual("B");
 	expect(arr[1].parent).toEqual(arr[0]);
 
-	expect(arr[1].methods.method1.name).toEqual("method1");
-	expect(arr[1].methods.method1.type).toEqual("void");
-	expect(arr[1].methods.method1.modifier[0]).toEqual("+");
-	expect(arr[1].methods.method1.parameters.length).toEqual(0);
+	expect(arr[1].methods.$method1.name).toEqual("method1");
+	expect(arr[1].methods.$method1.type).toEqual("void");
+	expect(arr[1].methods.$method1.modifier[0]).toEqual("+");
+	expect(arr[1].methods.$method1.parameters.length).toEqual(0);
 	
 	});
 
@@ -109,17 +109,17 @@ describe("Parser", function() {
 	expect(arr.length).toEqual(2);
 
 	expect(arr[0].name).toEqual("A");
-	expect(arr[0].methods.method1.name).toEqual("method1");
-	expect(arr[0].methods.method1.type).toEqual("void");
-	expect(arr[0].methods.method1.modifier[0]).toEqual("+");
-	expect(arr[0].methods.method1.parameters.length).toEqual(0);
+	expect(arr[0].methods.$method1.name).toEqual("method1");
+	expect(arr[0].methods.$method1.type).toEqual("void");
+	expect(arr[0].methods.$method1.modifier[0]).toEqual("+");
+	expect(arr[0].methods.$method1.parameters.length).toEqual(0);
 
 
 	expect(arr[1].name).toEqual("B");
-	expect(arr[1].methods.method2.name).toEqual("method2");
-	expect(arr[1].methods.method2.type).toEqual("int");
-	expect(arr[1].methods.method2.modifier[0]).toEqual("+");
-	expect(arr[1].methods.method2.parameters.length).toEqual(0);
+	expect(arr[1].methods.$method2.name).toEqual("method2");
+	expect(arr[1].methods.$method2.type).toEqual("int");
+	expect(arr[1].methods.$method2.modifier[0]).toEqual("+");
+	expect(arr[1].methods.$method2.parameters.length).toEqual(0);
 	
 	});
 
@@ -138,25 +138,25 @@ describe("Parser", function() {
 
 	expect(arr[0].name).toEqual("A");
 
-	expect(arr[0].attrs.attr1.name).toEqual("attr1");
-	expect(arr[0].attrs.attr1.type).toEqual("int");
-	expect(arr[0].attrs.attr1.modifier).toEqual("+");
+	expect(arr[0].attrs.$attr1.name).toEqual("attr1");
+	expect(arr[0].attrs.$attr1.type).toEqual("int");
+	expect(arr[0].attrs.$attr1.modifier).toEqual("+");
 
 	expect(arr[1].name).toEqual("B");
 
-	expect(arr[1].methods.method1.name).toEqual("method1");
-	expect(arr[1].methods.method1.type).toEqual("void");
-	expect(arr[1].methods.method1.modifier[0]).toEqual("+");
-	expect(arr[1].methods.method1.parameters.length).toEqual(0);
+	expect(arr[1].methods.$method1.name).toEqual("method1");
+	expect(arr[1].methods.$method1.type).toEqual("void");
+	expect(arr[1].methods.$method1.modifier[0]).toEqual("+");
+	expect(arr[1].methods.$method1.parameters.length).toEqual(0);
 
 	expect(arr[2].name).toEqual("C");
 	expect(arr[2].parent).toEqual(arr[0]);
 	expect(arr[2].interfaces[0].name).toEqual("B");
 
-	expect(arr[2].methods.method2.name).toEqual("method2");
-	expect(arr[2].methods.method2.type).toEqual("int");
-	expect(arr[2].methods.method2.modifier[0]).toEqual("+");
-	expect(arr[2].methods.method2.parameters.length).toEqual(0);
+	expect(arr[2].methods.$method2.name).toEqual("method2");
+	expect(arr[2].methods.$method2.type).toEqual("int");
+	expect(arr[2].methods.$method2.modifier[0]).toEqual("+");
+	expect(arr[2].methods.$method2.parameters.length).toEqual(0);
 	
 	});
 
@@ -174,16 +174,16 @@ describe("Parser", function() {
 
 	expect(arr[0].name).toEqual("A");
 
-	expect(arr[0].attrs.attr1.name).toEqual("attr1");
-	expect(arr[0].attrs.attr1.type).toEqual("int");
-	expect(arr[0].attrs.attr1.modifier).toEqual("+");
+	expect(arr[0].attrs.$attr1.name).toEqual("attr1");
+	expect(arr[0].attrs.$attr1.type).toEqual("int");
+	expect(arr[0].attrs.$attr1.modifier).toEqual("+");
 
 	expect(arr[1].name).toEqual("B");
 
-	expect(arr[1].methods.method1.name).toEqual("method1");
-	expect(arr[1].methods.method1.type).toEqual("void");
-	expect(arr[1].methods.method1.modifier[0]).toEqual("+");
-	expect(arr[1].methods.method1.parameters.length).toEqual(0);
+	expect(arr[1].methods.$method1.name).toEqual("method1");
+	expect(arr[1].methods.$method1.type).toEqual("void");
+	expect(arr[1].methods.$method1.modifier[0]).toEqual("+");
+	expect(arr[1].methods.$method1.parameters.length).toEqual(0);
 
 	expect(parser.out_err.length).toEqual(1);
 	expect(parser.out_err[0]).toContain("Invalid inheritenance! Only one superclass allowed.");
@@ -204,16 +204,16 @@ describe("Parser", function() {
 
 	expect(arr[0].name).toEqual("A");
 
-	expect(arr[0].attrs.attr1.name).toEqual("attr1");
-	expect(arr[0].attrs.attr1.type).toEqual("int");
-	expect(arr[0].attrs.attr1.modifier).toEqual("+");
+	expect(arr[0].attrs.$attr1.name).toEqual("attr1");
+	expect(arr[0].attrs.$attr1.type).toEqual("int");
+	expect(arr[0].attrs.$attr1.modifier).toEqual("+");
 
 	expect(arr[1].name).toEqual("B");
 
-	expect(arr[1].methods.method1.name).toEqual("method1");
-	expect(arr[1].methods.method1.type).toEqual("void");
-	expect(arr[1].methods.method1.modifier[0]).toEqual("+");
-	expect(arr[1].methods.method1.parameters.length).toEqual(0);
+	expect(arr[1].methods.$method1.name).toEqual("method1");
+	expect(arr[1].methods.$method1.type).toEqual("void");
+	expect(arr[1].methods.$method1.modifier[0]).toEqual("+");
+	expect(arr[1].methods.$method1.parameters.length).toEqual(0);
 
 	expect(parser.out_err.length).toEqual(1);
 	expect(parser.out_err[0]).toContain("Invalid association! Inheritance first, then interface implementation.");
